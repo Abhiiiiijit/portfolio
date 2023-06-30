5aafaf4a503a4eb5c66d248e7efc0d585f403e57
Abhiiiiijit <abhijeetbosedas560@gmail.com>
2023-06-30 19:55:47 +0530

    Basic Common CSS

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,18 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
 export default function Header() {
   return (
-    <div>
-      I am Header
-    </div>
-  )
+    // <div>
+    //   I am Header
+    // </div>
+    <>
+      <header id="header">
+        <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Abhijit</h1>
+        <h5>Web Developer</h5>
+        </div>
+      </header>
+    </>
+  );
 }
