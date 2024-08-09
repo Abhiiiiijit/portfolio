94453641859a420ef51ff17d0ee59bf8bcd8fcb1
Abhiiiiijit <abhijeetbosedas560@gmail.com>
2024-08-09 22:59:22 +0530

    CChnage in jobs

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -7,12 +7,25 @@ const Work = [
     id: 1,
     image: img1,
     title: "SDE Intern",
+    date:"2nd Jan, 2024 - 19th May, 2024",
     about_project:
-      "Frontend Intern using Flutter",
+      "Partner Onboarding of LOS Portal ",
+    location:"BKC, Mumbai, Maharastra, India",
+    linkedin: "https://www.linkedin.com/company/kuhoo-edufintech/mycompany/",
+    webstite: "https://www.kuhoo.com/",
+  },  
+  {
+    id: 2,
+    image: img1,
+    title: "SDE I",
+    date:"19th May, 2024 - Present",
+    about_project:
+      "Partner On-boarding and Agent On-boarding of LOS Portal, LLDs ",
     location:"BKC, Mumbai, Maharastra, India",
     linkedin: "https://www.linkedin.com/company/kuhoo-edufintech/mycompany/",
     webstite: "https://www.kuhoo.com/",
   },
+
 ];
 const Services = () => {
   return (
@@ -20,18 +33,19 @@ const Services = () => {
       <p style={{ textAlign: "center" }}>Jobs and Internships</p>
       <h2>Work</h2>
       <div className="container project_container">
-        {Work.map(({ id, image, title, about_project, location,linkedin, webstite }) => {
+        {Work.map(({ id, image, title, date,about_project, location,linkedin, webstite }) => {
           return (
             <article key={id} className="project_item">
               <div className="project_item-image">
                 <img src={image} alt={title} />
               </div>
-              <h3 style={{ textAlign: "center" }}>{title}</h3>
+              <h3 style={{ textAlign: "center" }}>{title}
+              <h6 style={{ marginTop: "2%", color: "gray" }}>{date}</h6></h3>
               <p style={{ marginTop: "1%", marginBottom: "3%" }}>
                 {about_project}
               </p>
               <p style={{ marginTop: "1%", marginBottom: "3%" }}>
-                {location}
+                <span style={{fontWeight:"bold"}}>Location:- </span>{location}
               </p>
               <div className="project-item-cta" style={{ display: "flex" }}>
                 <a
